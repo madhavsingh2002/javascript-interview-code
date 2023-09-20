c60ef3d371469c95b6290b724e5e6572259367dc
madhav <madhav201singh@gmail.com>
2023-09-20 18:59:11 +0530

    update

--- javascript-functions/Problem-1.js
+++ javascript-functions/Problem-1.js
@@ -1,1 +1,13 @@
-console.log('Problem-1')
+// Define a function called reverse_a_number that takes a numerical input n
+function reverse_a_number(n) {
+    // Convert the numerical input to a string by concatenating it with an empty string
+    n = n + "";
+
+    // Use the split() method to convert the string into an array of characters,
+    // reverse the order of the characters using reverse(), and then join them back into a string
+    return n.split("").reverse().join("");
+}
+
+// Call the reverse_a_number function with the input 32243, reverse the digits,
+// and then convert the result back to a number using Number()
+console.log(Number(reverse_a_number(32243)));
