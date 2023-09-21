1b5eae93c068a53ac5fe78cbcd36f2786c296808
madhav <madhav201singh@gmail.com>
2023-09-21 17:56:37 +0530

    concat

--- javascript-functions/Problem-2.js
+++ javascript-functions/Problem-2.js
@@ -1,10 +1,15 @@
-function uppercase(str) {
-    // Use a regular expression to find the first letter of each word and capitalize it
-    return str.replace(/\b\w/g, function(char) {
-      return char.toUpperCase();
-    });
-  }
-  
-  // Example usage: Convert the string "the quick brown fox" to title case
-  console.log(uppercase("the quick brown fox"));
-  
+// Let's the simple solution.....
+
+function uppercase(str){
+    // use a regular expression to find the first letter of each word and capitalize it..
+    return str.replace(/\b\w/g,function(char){
+        return char.toUpperCase();
+    })
+}
+// 
+console.log(uppercase('the quick brown fox'))// THank's for watchingg...
+
+// we use a regular expression (/\b\w/g) to find the first letter of each word 
+// (where \b matches word boundaries and \w matches word characters), and then 
+// we use the callback function to capitalize each found letter. This approach 
+// simplifies the code and achieves the same result.
